0aef52d1cd7944b1970256870b6c23a5227c63ca
whalewalker <mongodbms@gmail.com>
2021-10-08 14:12:21 +0100

    setup jest for testing

---
+++ README.md
@@ -0,0 +1,2 @@
+# Testing
+[Testing Javascript code with jest](https://blog.greenroots.info/setup-javascript-testing-with-jest-be-a-super-hero)
---
+++ src/calc.js
@@ -0,0 +1,7 @@
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const multi = (a, b) => a * b;
+const div = (a,b) => a/b;
+
+export { add, multi, sub, div };
